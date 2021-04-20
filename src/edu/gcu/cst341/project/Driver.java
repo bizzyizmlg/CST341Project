5033520881031648e8a83ced4c39c2e1d2c97af2
kingb <kingb@DESKTOP-011SDDV.home>
2021-04-20 02:55:01 -0400

    # WARNING: head commit changed in the meantime
    HKello

--- src/edu/gcu/cst341/project/Driver.java
+++ src/edu/gcu/cst341/project/Driver.java
@@ -2,7 +2,7 @@ package edu.gcu.cst341.project;
 
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		MyStore gcu = new MyStore("GCU Shop");
 		gcu.open();
 
--- src/edu/gcu/cst341/project/MyStore.java
+++ src/edu/gcu/cst341/project/MyStore.java
@@ -1,6 +1,7 @@
 package edu.gcu.cst341.project;
 
 import java.beans.Statement;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,34 +10,40 @@ import java.util.Scanner;
 
 public class MyStore {
 
+	Scanner sc = new Scanner(System.in);
 	private String name;
 	private DBConnect con;
+	private boolean cartOrder = false;
+	private String customer;
+	private int userId;
 
 	MyStore(String name) {
 		this.name = name;
 		con = new DBConnect();
 	}
 
-	public void open() {
+	public void open() throws Exception {
 		String user = null;
 		boolean exit = false;
 		do {
 			switch (UserInterface.menuMain()) {
 			case 0:
-				System.out.println("Thank you! Come again!");
+				System.out.println("...Exiting");
 				exit = true;
 				break;
 			case 1:
 				user = login();
 				if (user != null) {
-					System.out.println("Login successful!!");
+					System.out.println("...You are now logged in!!");
 					shop();
 				} else {
-					System.out.println("Login unsuccessful");
+					System.out.println("...Sorry, wrong password.");
 				}
 				break;
 			case 2:
+
 				admin();
+
 				break;
 			default:
 				open();
@@ -49,7 +56,7 @@ public class MyStore {
 
 		String[] login = UserInterface.login();
 
-		String sql = "SELECT UserId, UserFirstName FROM users WHERE UserName = ? AND UserPassword = ? AND UserStatus = 1";
+		String sql = "SELECT user_Id, user_First_Name FROM users WHERE user_Name = ? AND user_Password = ? AND user_Status = 1";
 
 		try (PreparedStatement ps = con.getConnection().prepareStatement(sql)) {
 			ps.setString(1, login[0]);
@@ -57,7 +64,10 @@ public class MyStore {
 			ResultSet rs = ps.executeQuery();
 
 			if (rs.next()) {
-				result = rs.getString("UserFirstName");
+				result = rs.getString("user_First_Name");
+				userId = rs.getInt("user_Id");
+				customer = rs.getString("user_First_Name");
+				System.out.println("Thank you, " + customer + " you are now logged in...");
 			} else {
 				result = null;
 			}
@@ -67,7 +77,7 @@ public class MyStore {
 		return result;
 	}
 
-	private void shop() {
+	private void shop() throws Exception {
 		switch (UserInterface.menuShop()) {
 		case 0:
 			return;
@@ -78,57 +88,65 @@ public class MyStore {
 			readCartItems();
 			break;
 		case 3:
-			deleteCartItem();
-			break;
-		default:
-			return;
-		}
-	}
-
-	private void admin() {
-		switch (UserInterface.menuAdmin()) {
-		case 0:
-			return;
-		case 1:
-			try {
-				createProduct();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			break;
-		case 2:
-			try {
-				readProducts();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			break;
-		case 3:
-			try {
-				updateProduct();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			break;
 		case 4:
-			try {
-				deleteProduct();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			deleteCartItem();
 			break;
 		default:
 			open();
 		}
 	}
 
-	private void createCartItem() {
-		System.out.println("Add (Create) item to cart...");
-		System.out.println();
+	private void admin() throws Exception {
+		switch (UserInterface.menuAdmin()) {
+		case 0:
+			return;
+		case 1:
+			createProduct();
+			break;
+		case 2:
+			readProducts();
+			break;
+		case 3:
+			updateProduct();
+			break;
+		case 4:
+			deleteProduct();
+			break;
+		default:
+			open();
+		}
+	}
+
+	private void createCartItem() throws Exception {
+
+		System.out.println("Add item to cart...");
+		System.out.println("================");
+		readProducts();
+		System.out.println("What is your user name Id?...");
+
+		int uId2 = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Please Enter the Product Id: ");
+		String pId2 = sc.nextLine();
+		System.out.println("Please Enter the Date: ");
+		String pTime = sc.nextLine();
+
+		String sql_command = "INSERT INTO cst341nproject.shoppingcart (user_Id, product_Id, "
+				+ "product_Added_Date_Time) VALUE (?, ?, ?);";
+
+		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql_command)) {
+
+			initiate.setInt(1, uId2);
+			initiate.setString(2, pId2);
+			initiate.setString(3, pTime);
+
+			initiate.executeUpdate();
+			System.out.println(initiate);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		readProducts();
 	}
 
 	private void readCartItems() {
@@ -136,143 +154,145 @@ public class MyStore {
 		System.out.println();
 	}
 
-	private void deleteCartItem() {
-		Scanner sc = new Scanner(System.in);
-
+	private void deleteCartItem() throws Exception {
 		System.out.println("Delete from cart...");
-		System.out.println();
+		System.out.println("===============");
+		System.out.println("Which item would you like to delete?...");
+		readCartItems();
+
+		int cart_Id = sc.nextInt();
+		sc.nextLine();
+
+		String sql4 = "DELETE FROM cst341nproject.shoppingcart WHERE user_Id = ?";
+
+		try (PreparedStatement initiate2 = con.getConnection().prepareStatement(sql4)) {
+
+			initiate2.setInt(1, cart_Id);
+			initiate2.executeUpdate();
+			System.out.println(initiate2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		admin();
 	}
 
-	private void createProduct() throws SQLException {
-		Scanner sc = new Scanner(System.in);
+	private void createProduct() throws Exception {
 
 		System.out.println("Create product...");
 		System.out.println("==================");
 		System.out.println("Please Enter a Unique Product Id: ");
-		int p_Id = sc.nextInt();
+		int pId = sc.nextInt();
 		sc.nextLine();
-		System.out.println("Please Enter the Products Name: ");
-		String p_Name = sc.nextLine();
+		System.out.println("Please Enter the Product Name: ");
+		String pName = sc.nextLine();
 		System.out.println("Please Enter the Products Price: ");
-		String p_Price = sc.nextLine();
-		System.out.println("Please Enter Products Inventory Status [true] = In stock [false] = Out of Stock : ");
-		String p_Status = sc.nextLine();
+		String pPrice = sc.nextLine();
+		System.out.println("Please Enter Products Inventory Status [true] = In stock [false] = Out of Stock: ");
+		String pStatus = sc.nextLine();
 
 		String sql_command = "INSERT INTO cst341nproject.products (product_Id, product_Name, "
-				+ "product_Price, product_Stock_Stadus) VALUE (?,?,?,?);";
+				+ "product_Price, product_Stock_Status) VALUE (?, ?, ?, ?);";
 
 		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql_command)) {
 
-			initiate.setInt(1, p_Id);
-			initiate.setString(2, p_Name);
-			initiate.setString(3, p_Price);
-			initiate.setString(4, p_Status);
+			initiate.setInt(1, pId);
+			initiate.setString(2, pName);
+			initiate.setString(3, pPrice);
+			initiate.setString(4, pStatus);
 			initiate.executeUpdate();
 			System.out.println(initiate);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		readProducts();
 	}
 
-	private void readProducts() throws SQLException {
+	private void readProducts() throws Exception {
 		System.out.println("View (Read) all products...");
 		System.out.println();
 
-		String sql_command = "SELECT product_Id, product_Name, product_Price, product_Stock_Stadus FROM cst341nproject.products;";
+		String sql_command = "SELECT product_Id, product_Name, product_Price, product_Stock_Status FROM cst341nproject.products;";
 
 		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql_command)) {
 
-			ResultSet list = initiate.executeQuery();
-			while (list.next()) {
-				System.out.println("All Products: " + list.getInt("product_Id") + " " + list.getString("product_Name")
-						+ " " + list.getString("product_Price") + " " + list.getString("product_Stock_Stadus"));
+			ResultSet rs = initiate.executeQuery();
+			while (rs.next()) {
+				System.out.println("ALL Products: " + rs.getInt("product_Id") + " " + rs.getString("product_Name") + " "
+						+ rs.getString("product_Price") + " " + rs.getString("product_Stock_Status"));
 			}
 		}
 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	private void updateProduct() throws SQLException {
-		Scanner sc = new Scanner(System.in);
+	private void updateProduct() throws Exception {
 
-		// 1] Ask user which credentials to update
-		System.out.println("Updating Product");
-		System.out.println("==================");
+		System.out.println("Update product...");
+		System.out.println("===========");
 		System.out.println("");
 
-		System.out.println("Which Product Id Number do you want to update? ");
+		System.out.println("Which Product Id Number would you like to update? ");
 		int id = sc.nextInt();
 		sc.nextLine();
 
-		// 2] Connect to DB and get current credentials
-		String sql = "SELECT product_Id, product_Name, product_Price, product_Stock_Stadus"
+		String sql = "SELECT product_Id, product_Name, product_Price, product_Stock_Status"
 				+ " FROM cst341nproject.products WHERE product_Id = ?";
-		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql)){
+		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql)) {
 
 			initiate.setInt(1, id);
 			ResultSet results = initiate.executeQuery();
 			results.next();
-			
-			
-		// 3] Display the current credentials while asking for new info
 
-		System.out.println("Update Product Id Number : [" + results.getInt("product_Id") + "] ? : ");
-		int p_Id = sc.nextInt();
-		sc.nextLine();
-		System.out.println("Update Product Name : [" + results.getString("product_Name") + "] ? : ");
-		String p_Name = sc.nextLine();
-		System.out.println("Update Product Price : [" + results.getString("product_Price") + "] ? : ");
-		String p_Price = sc.nextLine();
-		System.out.println("Update Inventory Status [true] = In stock [false] = Out of Stock : ["
-				+ results.getString("product_Stock_Stadus") + "] ? : ");
-		String inventory = sc.nextLine();
+			System.out.println("Update Product Id Number : [" + results.getInt("product_Id") + "] ? : ");
+			int pId = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Update Your Product Name : [" + results.getString("product_Name") + "] ? : ");
+			String pName = sc.nextLine();
+			System.out.println("Update Your Product Price : [" + results.getString("product_Price") + "] ? : ");
+			String pPrice = sc.nextLine();
+			System.out.println("Update Inventory Status [true] = In Stock [false] = Out of Stock : ["
+					+ results.getString("product_Stock_Status") + "] ? : ");
 
-		// 4] Write the updated credentials to the DB
-		
-		String sql2 = "UPDATE cst341nproject.products SET product_Id = ?, product_Name = ?, product_Price = ?, "
-				+ "product_Stock_Stadus = ? WHERE product_Id = 131";
-		try (PreparedStatement initiate1 = con.getConnection().prepareStatement(sql2)){
-		
-		initiate1.setInt(1, p_Id);
-		initiate1.setString(2, p_Name);
-		initiate1.setString(3, p_Price);
-		initiate1.setString(4, inventory);
+			String inventory = sc.nextLine();
 
-		initiate1.executeUpdate();
+			String sql1 = "UPDATE cst341nproject.products SET product_Id = ?, product_Name = ?, product_Price = ?, "
+					+ "product_Stock_Status = ? WHERE product_Id = 132";
 
-		// 5] Check to see if the data was written to the DB throws exception
-		String sql3 = "SELECT * FROM cst341nproject.products WHERE product_Id = ?";
-		try (PreparedStatement initiate2 = con.getConnection().prepareStatement(sql3)){
-			initiate2.setInt(1, id);
-		ResultSet results3 = initiate2.executeQuery();
-		results3.next();
-		}
-		
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		}
+			try (PreparedStatement initiate1 = con.getConnection().prepareStatement(sql1)) {
+
+				initiate1.setInt(1, pId);
+				initiate1.setString(2, pName);
+				initiate1.setString(3, pPrice);
+				initiate1.setString(4, inventory);
+
+				initiate1.executeUpdate();
+
+				String sql2 = "SELECT * FROM cst341nproject.products WHERE product_Id = ?";
+				try (PreparedStatement initiate2 = con.getConnection().prepareStatement(sql2)) {
+					initiate2.setInt(1, id);
+					ResultSet results3 = initiate2.executeQuery();
+					results3.next();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
 
 	}
 
 	private void deleteProduct() throws SQLException {
 
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Delete Product: ");
-		System.out.println("===================");
-		System.out.println("What Product Id would you like to delete? ");
+		System.out.println("Delete product...");
+		System.out.println("=============");
+		System.out.println("Which Product Id Would You Like To Delete? ");
 		int product_Id = sc.nextInt();
 		sc.nextLine();
 
-		String sql12 = "DELETE FROM cst341nproject.products WHERE product_Id = ?";
+		String sql3 = "DELETE FROM cst341nproject.products WHERE product_Id = ?";
 
-		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql12)) {
+		try (PreparedStatement initiate = con.getConnection().prepareStatement(sql3)) {
 
 			initiate.setInt(1, product_Id);
 			initiate.executeUpdate();
@@ -280,10 +300,6 @@ public class MyStore {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		// Now you can extract all the records
-		// to see the remaining records
-
 	}
 
 }
